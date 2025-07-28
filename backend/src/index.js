a467f27b09a51c9642d252ba8bf2484aafe41c81
Piyush Gupta <piyushgupta1165@gmail.com>
2025-07-28 16:50:57 +0530

    fixed login api

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,26 +5,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user.route.js');
 // const messageRoutes = require('./routes/message.route.js');
-// const chatRoutes = require('./controllers/chat.controller.js');
-// const cors = require('cors');
+const chatRoutes = require('./routes/chat.route.js');
+const cors = require('cors');
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(cors({
-//   origin: '*', 
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true 
-// }));
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true 
+}));
 
 
 app.use(bodyParser.json());
 
 app.use("/user", userRoutes);
 // app.use('/message', messageRoutes);
-// app.use('/chat', chatRoutes);
+app.use('/chat', chatRoutes);
 
 
 app.get('/', (req, res) => {
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -4,8 +4,8 @@ const {register, login, logout} = require('../controllers/user.controller.js');
 const router = express.Router();
 
 router.route('/register').post(register);
-router.route('/login').get(login);
-router.route('/logout').get(logout);
+router.route('/login').post(login);
+router.route('/logout').post(logout);
 
 
 module.exports = router;
