85c86abbe7f0ab65ae75243b866577f7b3fdeb32
Piyush Gupta <piyushgupta1165@gmail.com>
2025-07-30 18:03:52 +0530

    updated cors configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin: [ 'http://localhost:5173'], 
+  origin: [ 'http://localhost:5173', 'https://ai-model-vjyo.onrender.com'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   
   credentials: true 
